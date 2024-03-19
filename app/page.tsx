94e84b701a3c98897699a396150dd35981c9edb5
ilorez <zobirnajdaoui@gmail.com>
2024-03-19 11:23:07 +0000

    set up projet with nextjs typescript tailwindcss shadcn-ui lucide-react

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { ClipboardCheck } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div>
+      <Button> <ClipboardCheck /> Click me</Button>
+    </div>
+  );
+}
