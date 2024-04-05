f1d8b9cda204c86188e9c92d6d8898d12e279adf
ilorez <zobirnajdaoui@gmail.com>
2024-04-05 21:23:22 +0000

    feat: add ImageFormCard and create connect to firebase
    install firebase and create env variables for firebase app, create ImageFormCard for upload image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { ClipboardCheck } from "lucide-react";
-
+import ImageCardForm from "@/components/Card";
+import {app} from "@/firebase/firebase";
 export default function Home() {
   return (
-    <div>
-      <Button> <ClipboardCheck /> Click me</Button>
-    </div>
+    <section className="w-screen h-screen flex justify-center items-center">
+      <ImageCardForm/>
+    </section>
   );
 }
